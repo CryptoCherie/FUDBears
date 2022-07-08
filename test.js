f1860db51996e066610b02795e4703804b71df46
CryptoCherie <whatiscrypto2021@gmail.com>
2022-07-08 11:14:49 -0700

    first commit

---
+++ test.js
@@ -0,0 +1,15 @@
+function myFunctions(){
+    console.log ("walletConnect");
+    alert ("walletConnect");
+  }
+
+  async function connect() {
+    if (window.ethereum) {
+    
+     await window.ethereum.request({ method: "eth_requestAccounts" });
+     window.web3 = new Web3(window.ethereum);
+   
+    } else {
+     console.log("No wallet");
+    }
+   }
